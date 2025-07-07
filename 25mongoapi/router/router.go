9d53b4bf10506614dcce6b387422883303e030f7
Faiz Faiyaz <forcourse23@gmail.com>
2025-07-07 15:05:37 +0530

    temp

--- 25mongoapi/router/router.go
+++ 25mongoapi/router/router.go
@@ -9,6 +9,10 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/movie", controller.GetMyAllMovie).Methods("GET")
+	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}", controller.DeleteOneMovie).Methods("DELETE")
+	router.HandleFunc("/api/movie", controller.DeleteALLMovie).Methods("DELETE")
 
 	return router
 }
